c8551542f2b9cfd69a87d7822c2a13d9e6a3d4e7
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2024-10-23 14:21:56 +0200

    Added RateLimitData

---
+++ RateLimiter/Data/RateLimitData.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RateLimiter.Data
+{
+    public class RateLimitData
+    {
+        public RateLimitData(DateTime lastResponseTime, int numberOfRequests)
+        {
+            LastResponseTime = lastResponseTime;
+            NumberOfRequests = numberOfRequests;
+        }
+        [JsonProperty(PropertyName = "LastResponse")]
+        public DateTime LastResponseTime { get; private set; }
+        public int NumberOfRequests { get; private set; }
+         public bool IsMaximumRequestTrasholdReached(int timeWindowInSeconds, int maxRequests)
+            => DateTime.UtcNow < LastResponseTime.AddSeconds(timeWindowInSeconds) && NumberOfRequests == maxRequests;
+    }
+}
